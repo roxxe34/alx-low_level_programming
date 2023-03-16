57dd1754ef9e90f46cd33b5a203d251b1fd0d1e0
roxxe34 <hamzafarissi7@gmail.com>
2023-03-16 11:54:01 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 /**
  *  Hello, puts 
  */
-int main(void) { 
-
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }
