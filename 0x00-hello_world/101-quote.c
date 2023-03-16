9002d76697f59749cad91914d5ab5eb83be9bbd0
roxxe34 <hamzafarissi7@gmail.com>
2023-03-16 14:04:24 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,6 @@
 */
 int main(void)
 {
-char message[] = "and that piece of art is useful\" - "
-  "Dora Korpar, 2015-10-19\n";
-int len = sizeof(message) - 1;
-write(STDERR_FILENO, message, len);
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
