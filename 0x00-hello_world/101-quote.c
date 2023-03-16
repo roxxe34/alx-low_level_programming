0d214951f01998b538761e964d4741e694e69de4
roxxe34 <hamzafarissi7@gmail.com>
2023-03-16 13:47:23 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
 #include <unistd.h>
+
 /**
-* main - Print (with proper grammar, but the outcome is a piece of art,)
+* main - Entry point
 *
-* Return: Always 0 (Success)
+* Return: Always 1 (error code)
 */
 int main(void)
 {
 char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-ssize_t len = sizeof(message) - 1;
-ssize_t n = write(STDERR_FILENO, message, len);
-if (n != len)
-return(1);
-return(0);
+int len = sizeof(message) - 1;
+write(STDERR_FILENO, message, len);
+return (1);
 }
