aa611b6bc3579909cd0f12d055fdb6cb47bc6784
roxxe34 <hamzafarissi7@gmail.com>
2023-03-16 13:36:18 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
-* main - Print (with proper grammar, but the outcome is a piece of art,)
-*
-* Return: Always 0 (Success)
-*/
+ * main - Print (with proper grammar, but the outcome is a piece of art,)
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-for (int i = 0; message[i] != '\0'; i++)
-putchar(message[i]);
-putchar('\n');
-return (1);
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ssize_t len = 0;
+while (message[len] != '\0')
+len++;
+ssize_t n = write(STDERR_FILENO, message, len);
+return (n != len);
 }
