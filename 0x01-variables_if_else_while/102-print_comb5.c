a9d922bab80e7aa5b31c17e26bba1901f588b776
roxxe34 <hamzafarissi7@gmail.com>
2023-03-17 17:46:11 -0700

    done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,21 +12,14 @@ for (x = 0; x < 100; x++)
 {
 for (y = 0; y <= 100; y++)
 {
-if (x < y)
-{
 putchar((x / 10) + 48);
 putchar((x % 10) + 48);
 putchar(' ');
 putchar((y / 10) + 48);
 putchar((y % 10) + 48);
-{
 putchar(',');
 putchar(' ');
-}
-{
 putchar('\n');
-}
-}
 }
 }
 return (0);
