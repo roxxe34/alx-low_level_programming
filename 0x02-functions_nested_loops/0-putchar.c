7a97a5abfab980192f6074671da4ce6a74361a31
roxxe34 <hamzafarissi7@gmail.com>
2023-03-21 13:24:25 -0700

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,18 @@
-#include <main.h>
+#include <unistd.h>
+#include "main.h"
 /**
- * main - printing _putchar folowed by new line
+ * main - entry poiny
+ * print _putchar, followed by a new line.
  *
- * Return: Always 0 (Success)
+ * Return: On success 0
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int main(void)
 {
-printf("_putchar\n");
+char word[8] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+_putchar(word[i]);
+_putchar('\n');
 return (0);
 }
