fe0163ad12de4598e21ea5762377cc7d6ec2577f
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-03-22 00:41:47 +0000

    Create 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include <main.h>
+/**
+ * print_sign - Enter point
+ * @n: carrier variable
+ * Description:prints the sign of the number
+ * Return: 1 if n>0, 0 if n == 0, -1 if n<0
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+printf("+");
+return 1;
+} else if (n == 0)
+{
+printf("0");
+return 0;
+} else
+{
+printf("-");
+return -1;
+}
+}
