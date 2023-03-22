c4f703221a114b02059474db4275fbf53bd1c0e3
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-03-22 00:33:39 +0000

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,16 +3,16 @@
 /**
  * _islower - Entry point
  * Description:checks is a character is lowercase
- *
+ * @c: the integer value it receives
  * Return: 1 if true. 0 if false.
  */
 int _islower(int c)
 {
-int i = 'a';
-for (i = 'a'; i <= 'z'; i++)
+int y = 'a';
+for (y = 'a'; y <= 'z'; y++)
 {
 /* refer int c*/
-if (c == i)
+if (c == y)
 {
 return (1);
 }
