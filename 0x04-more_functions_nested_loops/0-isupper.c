7c7cde30fbb1bf4909301e1e3bce8512b8464149
roxxe34 <hamzafarissi7@gmail.com>
2023-03-23 09:57:40 -0700

    done

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,13 +4,13 @@
  * @c: carrier integer variable
  * Return: 1 if true, 0 if false
  */
-int _isupper(int c);
+int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
 {  
-return 1;
+return (1);
 }
 else {
-return 0;
+return (0);
 }
 }
