7368373c0528e2218a5a6e99f92ce0ecb1ef4bba
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-03-24 01:39:20 +0000

    Create 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * return 0
+ */
+int main()
+{
+long long int num = 612852475143;
+long long int largest_prime_factor = 2;
+while (num > largest_prime_factor)
+{
+if (num % largest_prime_factor == 0)
+{
+num = num / largest_prime_factor;
+}
+else 
+{
+largest_prime_factor++;
+}
+}
+printf("%lld\n", largest_prime_factor);
+return 0;
+}
