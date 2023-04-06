f2cffcd52fecc71f790599dce75351cf91c5f933
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-04-06 02:38:02 +0000

    fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,12 +2,11 @@
 
 /**
 * _sqrt_recursion - returns the natural square root of a number
-* @n: number to be used
+* @n: number 
 * _sqrt - returns the square root of a number
 * @x: squared number
 * Return: the square root of n
 */
-
 int _sqrt_recursion(int n)
 {
 	if (n == 0 || n == 1)
@@ -19,10 +18,8 @@ int _sqrt_recursion(int n)
  * _sqrt - returns the square root of a number
  * @n: test number
  * @x: squared number
- *
  * Return: the square root of n
  */
-
 int _sqrt(int n, int x)
 {
 	if (n > x / 2)
