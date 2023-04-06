d574ea8117237529ff2b3d39dc46e8a2b04365e0
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-04-06 02:43:04 +0000

    done

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -16,6 +16,11 @@ int is_palindrome(char *s)
 
 	return (check_palindrome(s, len, index));
 }
+/**
+ * find_strlen - Returns the length of a string
+ * @s: The string
+ * Return: The length of the string
+ */
 int find_strlen(char *s)
 {
 	int len = 0;
@@ -28,6 +33,14 @@ int find_strlen(char *s)
 
 	return (len);
 }
+/**
+ * check_palindrome - Checks if a string is a palindrome
+ * @s: The string
+ * @len: The length of s
+ * @index: The index of the string to be checked
+ * Return: If the string is a palindrome (- 1)
+ *If the string is not a palindrome (- 0)
+ */
 int check_palindrome(char *s, int len, int index)
 {
 	if (s[index] == s[len / 2])
