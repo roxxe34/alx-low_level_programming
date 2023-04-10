3bd587b71f068bdae332d5b03ed8b078d0266c28
roxxe34 <hamzafarissi7@gmail.com>
2023-04-10 09:10:14 -0700

    done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,6 +6,13 @@
  */
 int main(int argc, char *argv[])
 {
-printf("%s\n", argv[0]);
+if (argc > 1)
+{
+printf("The first argument is: %s\n", argv[1]);
+}
+else
+{
+printf("No arguments provided.\n");
+}
 return 0;
 }
