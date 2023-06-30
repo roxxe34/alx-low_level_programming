f85dc197c7c24bc54bb9f19942ee81ba1186101d
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-07-01 00:15:55 +0100

    Create 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * before_main - Function executed before the main function
+ */
+void __attribute__((constructor)) before_main()
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
