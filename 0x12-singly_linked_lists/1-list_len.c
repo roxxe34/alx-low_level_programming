23918312eb0da1ae041bec10121fcd25d3b6a037
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-06-30 19:55:03 +0100

    Create 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "list.h"
+/**
+ * list_len - returns the number of elements in a linked 
+ * @h: A pointer to the head of the list
+ *
+ * Return: The number of nodes in the list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+while(h != NULL)
+{
+	len++;
+	h = ->next;
+}
+return len;
+}
