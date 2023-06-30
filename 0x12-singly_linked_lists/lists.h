3a7368e968313a3f35cc88533bf66a1740353288
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-06-30 23:39:29 +0100

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,20 @@
 #include <stddef.h>
 #include <stdio.h>
 /**
+ * _strlen - function that returns the length of a string.
+ * @s : s is a character
+ * Return: value is i
+ */
+int _strlen(const char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+return (i);
+}
+/**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
