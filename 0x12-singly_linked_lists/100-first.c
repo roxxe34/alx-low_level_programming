f4e1db36ac5412f0519967899a42d8cd79c4c14b
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-07-01 00:18:27 +0100

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,5 +5,5 @@
 void __attribute__((constructor)) before_main()
 {
 printf("You're beat! and yet, you must allow,\n"
-        "I bore my house upon my back!\n");
+"I bore my house upon my back!\n");
 }
