e46fba801ad2fbed584792de0730c8282740097b
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-07-04 11:57:56 +0100

    Create 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+	* print-listint - prints all the elements of list
+	* 
+	* return: number of nodes
+*/
+size_t print_listint(const listint_t *h){
+int nodecount = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+nodecount++;
+h->next;
+}
+return (nodecount);
+}
