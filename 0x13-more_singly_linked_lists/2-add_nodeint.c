562be4643956f23c79d0b42233f0a5ca83ee5c66
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-07-04 12:17:35 +0100

    Create 2-add_nodeint.c

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_nodeint - returns the number of elements in a linked
+ * @h: A pointer to the head of the list
+ *
+ * Return: new node
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *newnode = malloc(sizeof(listint_t));
+newnode->n = n;
+newnode->next = *head;
+newnode = *head;
+return (newnode);
+}
