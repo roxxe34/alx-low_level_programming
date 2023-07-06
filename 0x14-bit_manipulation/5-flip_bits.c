9c0de7bf48e6a01a8131f43a5133e6889f956503
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-07-07 00:43:47 +0100

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -7,14 +7,12 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned long int diff = n ^ m;
-    unsigned int count = 0;
-
-    while (diff != 0)
-    {
-        count += diff & 1;
-        diff >>= 1;
-    }
-
-    return count;
+unsigned long int diff = n ^ m;
+unsigned int count = 0;
+while (diff != 0)
+{
+count += diff & 1;
+diff >>= 1;
+}
+return (count);
 }
