272b03bc6adea1770789ab030d37af4002388755
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-07-11 00:02:20 +0100

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@ if (filename == NULL)
 {
 return (-1);
 }
-file = open(filename, O_RDWR | O_APPEND, 0644);
+file = open(filename, O_WRONLY | O_APPEND);
 if (file == -1)
 {
 return (-1);
@@ -24,11 +24,14 @@ if (text_content != NULL)
 for (len = 0; text_content[len];)
 len++;
 }
+if (len > 0)
+{
 fwrite = write(file, text_content,  len);
 if (fwrite == -1 || fwrite < len)
 {
 close(file);
 return (-1);
+}
 }
 close(file);
 return (1);
