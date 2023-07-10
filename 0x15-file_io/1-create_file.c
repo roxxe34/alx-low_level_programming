e0d1ab8464ae525071f2b4934100234e3f7b3830
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-07-10 23:21:00 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,9 +9,7 @@
 int create_file(const char *filename, char *text_content)
 {
 ssize_t bytes_written;
-int truncate_result;
-int file;
-int len = 0;
+int truncate_result, file, len = 0;
 if (filename == NULL)
 {
 return (-1);
