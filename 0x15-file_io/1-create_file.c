5d8a9e3d204a602444ea8c23b2c1333317e77290
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-07-10 23:19:07 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,24 +6,25 @@
   *
   * Return: 1 if success
   */
-int create_file(const char *filename, char *text_content) {
+int create_file(const char *filename, char *text_content)
+{
 ssize_t bytes_written;
 int truncate_result;
 int file;
 int len = 0;
 if (filename == NULL)
 {
-return -1;
+return (-1);
 }
 file = open(filename, O_WRONLY | O_CREAT, 0600);
 if (file == -1) {
-return -1;
+return (-1);
 }
 truncate_result = truncate(filename, 0);
 if (truncate_result == -1)
 {
 close(file);
-return -1;
+return (-1);
 }
 while(text_content[len] != '\0')
 {
@@ -35,7 +36,7 @@ bytes_written = write(file, text_content, len);
 if (bytes_written == -1)
 {
 close(file);
-return -1;
+return (-1);
 }
 }
 else 
@@ -44,9 +45,9 @@ else
 	bytes_written = write(file, buffer, sizeof(buffer));
 	if (bytes_written == -1) {
 	close(file);
-	return -1;
+	return (-1);
 }
 }
 close(file);
-return 1;
+return (1);
 }
