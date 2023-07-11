6c1c53226b08e24a1a8682b8d259ccfccc3be96d
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-07-11 01:55:27 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,20 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define BUFFER_SIZE 1024
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 
-int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void print_error(const char* message);
-ssize_t cp(const char *file_from, const char *file_to);
 
 #endif /* MAIN_H */
