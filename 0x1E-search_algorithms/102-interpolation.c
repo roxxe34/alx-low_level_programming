8a143232755612331622ce983a225b053a0754e6
Hamza Farissi <hamzafarissi7@gmail.com>
2023-12-12 22:55:38 +0100

    done

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -34,22 +34,3 @@ int interpolation_search(int *array, size_t size, int value) {
 	printf("Value checked array[%d] is out of range\n", low);
 	return -1;
 }
-
-
-/**
- * main - Entry point
- *
- * Return: Always EXIT_SUCCESS
- */
-int main(void)
-{
-	int array[] = {
-			0, 0, 1, 2, 2, 2, 2, 3, 3, 4, 4, 5, 6, 6, 7, 8, 8, 8, 9, 9
-	};
-	size_t size = sizeof(array) / sizeof(array[0]);
-
-	printf("Found %d at index: %d\n\n", 3, interpolation_search(array, size, 3));
-	printf("Found %d at index: %d\n\n", 7, interpolation_search(array, size, 7));
-	printf("Found %d at index: %d\n", 999, interpolation_search(array, size, 999));
-	return (EXIT_SUCCESS);
-}
