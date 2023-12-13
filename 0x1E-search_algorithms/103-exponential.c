d4c99e4e10c5b4933a5a702972a69bd6c8a67881
Hamza Farissi <hamzafarissi7@gmail.com>
2023-12-13 22:41:00 +0100

    done

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,2 +1,71 @@
 #include "search_algos.h"
-int exponential_search(int *array, size_t size, int value);
+/**
+  * binary_search_high - Searches for a value in a sorted array
+  * @array: A pointer to the first element of the array to search.
+  * @low: The starting index of the [sub]array to search.
+  * @high: The ending index of the [sub]array to search.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL
+  */
+
+int binary_search_high(int *array, size_t low, size_t high, int value)
+{
+	int mid = 0;
+	size_t i;
+
+	while (low <= high)
+	{
+		printf("Searching in array: ");
+		for (i = low; i < high; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		mid = (low + high) / 2;
+		if (array[mid] < value)
+		{
+			low = mid + 1;
+		}
+		else if (array[mid] > value)
+		{
+			high = mid - 1;
+		}
+		else if (array[mid] == value)
+		{
+			return (mid);
+		}
+	}
+	return (-1);
+}
+/**
+  * exponential_search - Searches for a value in a sorted array
+  *                      of integers using exponential search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the index where the value is located.
+  *
+  * Description: Prints a value every time it is compared in the array.
+  */
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t i = 0, right;
+
+	if (array == NULL)
+		return (-1);
+
+	if (array[0] != value)
+	{
+		for (i = 1; i < size && array[i] <= value; i = i * 2)
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	}
+
+	right = i < size ? i : size - 1;
+	printf("Value found between indexes [%ld] and [%ld]\n", i / 2, right);
+
+	return (binary_search_high(array, i / 2, right, value));
+
+
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,4 +9,6 @@ int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int binary_search_high(int *array, size_t low, size_t high, int value);
+
 #endif /* SEARCH_ALGOS_H */
